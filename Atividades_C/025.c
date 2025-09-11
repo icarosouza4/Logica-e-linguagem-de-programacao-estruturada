333527746d2561fbbed802df52214f76e4d1a8bd
Seu Nome <icarosouza04@outlook.com>
2025-09-11 13:26:16 -0300

    Estrutura condicional simples

---
+++ Atividades_C/025.c
@@ -0,0 +1,16 @@
+// Estrutura condicional simples
+
+#include <stdio.h>
+
+int main()
+{
+    float m;
+
+    printf("Insira a nota: ");
+    scanf("%f", &m);
+
+    if(m >= 7.0)
+    {
+        printf("Aprovado(a)!\n");
+    }
+}
